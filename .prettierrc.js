8743221d78b765067fb22ffab5d7af4bf09885ca
lwp2333 <1348638869@qq.com>
2021-12-21 16:19:58 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'crlf',
+}
