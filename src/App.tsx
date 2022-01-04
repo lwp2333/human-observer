5d571cb1985522dce967b36845d4425aeec776d5
lwp2333 <1348638869@qq.com>
2022-01-04 16:19:09 +0800

    feat: 完善字体缺失

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, extendTheme } from 'native-base';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Routes from './routes';
 
 export default function App() {
+  const theme = extendTheme({
+    fontConfig: {
+      Roboto: {
+        100: {
+          normal: 'Roboto-Light',
+          italic: 'Roboto-LightItalic',
+        },
+        200: {
+          normal: 'Roboto-Light',
+          italic: 'Roboto-LightItalic',
+        },
+        300: {
+          normal: 'Roboto-Light',
+          italic: 'Roboto-LightItalic',
+        },
+        400: {
+          normal: 'Roboto-Regular',
+          italic: 'Roboto-Italic',
+        },
+        500: {
+          normal: 'Roboto-Medium',
+        },
+        600: {
+          normal: 'Roboto-Medium',
+          italic: 'Roboto-MediumItalic',
+        },
+      },
+    },
+    // Make sure values below matches any of the keys in `fontConfig`
+    fonts: {
+      heading: 'Roboto',
+      body: 'Roboto',
+      mono: 'Roboto',
+    },
+  });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <SafeAreaProvider>
         <Routes />
       </SafeAreaProvider>
